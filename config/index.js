9e08701fa7637b557ebb1c0c2df26994bbb09f7c
Juan La Jara <wxomb@Juans-MacBook.local>
2015-12-10 14:07:00 -0500

    add new attachments path

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports =
 { onEmailUrl      : process.env.TELLER_ON_EMAIL_URL     || 'http://localhost:5000/api/answers/'
-, attachmentsPath : process.env.TELLER_ATTACHMENTS_PATH || 'attachments/'
+, attachmentsPath : process.env.TELLER_ATTACHMENTS_PATH || '../attachments/'
 };

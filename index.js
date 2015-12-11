4fc9722d5861c61d8190cf4047bb185b95d9dd1e
Juan La Jara <wxomb@Juans-MacBook.local>
2015-12-11 17:46:19 -0500

    test no attachment

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express     = require('express');
 var compression = require('compression');
-// var bodyParser  = require('body-parser');
+var bodyParser  = require('body-parser');
 var multer      = require('multer');
 require('console-stamp')(console);
 
@@ -9,8 +9,8 @@ var config = require('./config');
 var app    = express();
 var upload = multer({ dest: config.attachmentsPath });
 
-// app.use( bodyParser.json({limit: '50mb'}) );
-// app.use( bodyParser.urlencoded({extended: true}) );
+app.use( bodyParser.json({limit: '50mb'}) );
+app.use( bodyParser.urlencoded({extended: true}) );
 app.use( compression() );
 
 

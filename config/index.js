c77f1a92a0dfc709d42926cb503f1f1ef596bbe7
Juan La Jara <wxomb@Juans-MacBook.local>
2016-01-08 12:11:47 -0500

    change max file size

--- config/index.js
+++ config/index.js
@@ -1,5 +1,5 @@
 module.exports =
 { onEmailUrl      : process.env.TELLER_ON_EMAIL_URL     || 'http://localhost:5000/api/answers/'
 , attachmentsPath : process.env.TELLER_ATTACHMENTS_PATH || '../attachments/'
-, maxFileSize     : 10 * 10 * 1024
+, maxFileSize     : 10 * 10 * 10 * 1024
 };
